eb7997682be266cd12b33d74c90ce59f2fd5404e
codebymatt <mattcraig365@gmail.com>
2018-09-10 19:04:52 +0100

    Complete ex_2.3

--- ch02/ex_2.3.c
+++ ch02/ex_2.3.c
@@ -15,8 +15,7 @@ int main()
 
     while((len = readline(s, MAXLENGTH)) > 0) {
         int i = htoi(s);
-        printf("Integer value is: %d\n\n", i);
-        //printf("String value is: %s\n\n", s);
+        printf("Decimal value is: %d\n\n", i);
     }
     return 0;
 }
@@ -36,4 +35,30 @@ int readline(char s[], int lim)
 
 int htoi(char s[])
 {
+    int i, n, len, multiplier;
+    char c;
+    i = n = len = 0;
+    multiplier = 1;
+
+    while(s[len] != '\0') {
+        len++;
+    }
+
+    if(s[i] == '0' && (s[i+1] == 'x' || s[i+1] == 'X')) {
+        i+=2;
+    }
+
+    while(len - 1 >= i) {
+        c = s[len-1];
+        if (c >= '0' && c <= '9') {
+            c = c - '0';
+        } else {
+            c = toupper(c) - 'A' + 10;
+        }
+        n = n + (multiplier * c);
+
+        len--;
+        multiplier *= 16;
+    }
+    return n;
 }
