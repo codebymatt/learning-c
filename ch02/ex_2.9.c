544b841ec314618401039746947efd50146f42a4
codebymatt <mattcraig365@gmail.com>
2018-10-29 22:37:43 +0000

    Complete ex_2.9

--- ch02/ex_2.9.c
+++ ch02/ex_2.9.c
@@ -1,13 +1,30 @@
 #include <stdio.h>
 
-// Use this observation to write a faster version of bitcount .
-
 // x - 1 will always have a different value in the rightmost bit compared to x, but all the other
 // bits will be the same (except the second to last rightmost bit at times).
 // Using the '&' operator means these differing bits will resolve to 0 in x,
 // While all other bits in x will remain the same.
 
+int bitcount(unsigned x);
+
 int main()
 {
+    unsigned x;
+    
+    scanf("%u", &x);
+    printf("%u\n", bitcount(x));
+
     return 0;
+}
+
+int bitcount(unsigned x)
+{
+    int b;
+
+    for (b = 0; x != 0; x &= (x - 1)) {
+        if (x | 0) {
+            b++;
+        }
+    }
+    return b;
 }
