262a57f6c58f0b0811480d05582cc28b83dae068
codebymatt <mattcraig365@gmail.com>
2018-10-30 20:03:21 +0000

    Complete ex_2.10

--- ch02/ex_2.10.c
+++ ch02/ex_2.10.c
@@ -1,4 +1,9 @@
 // Ex: Rewrite the function lower, which converts upper case letters to lower case, 
 // with a conditional expression instead of if-else .
 
-#include <stdio.h>
+// Trivial example so not going to bother with input and output
+
+int lower(int c)
+{
+    return (c >= 'A' && c <= 'Z') ? (c + 'a' - 'A') : c;
+}
