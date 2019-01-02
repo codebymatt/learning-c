125c02c3277a1757a299bcdf9f38920f187b8b23
codebymatt <mattcraig365@gmail.com>
2019-01-02 12:56:53 +0000

    Reimplement calculator boilerplate

--- ch04/ex_4.03.c
+++ ch04/ex_4.03.c
@@ -2,59 +2,52 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-#define MAXOP   100
-#define NUMBER  '0'
-#define MAXVAL  100
+#define MAXOP 100
+#define NUMBER '0'
+#define MAXVAL 100
 #define BUFSIZE 100
 
-int getop(char[]);
+int getop(char []);
 void push(double);
 double pop(void);
-int getch(void);
-void ungetch(int);
 
 int main()
 {
-    int type;
-    double op2;
-    char s[MAXOP];
+  int type;
+  double op2;
+  char s[MAXOP];
 
-    while ((type = getop(s)) != EOF) {
-        switch (type) {
-            case NUMBER:
-                push(atof(s));
-                break;
-            case '+':
-                push(pop() + pop());
-                break;
-            case '*':
-                push(pop() * pop());
-                break;
-            case '-':
-                op2 = pop();
-                push(pop() - op2);
-                break;
-            case '/':
-                op2 = pop();
-                if (op2 != 0.0) {
-                    push(pop() / op2);
-                } else {
-                    printf("error: zero divisor\n");
-                }
-                break;
-            case '%':
-                op2 = pop();
-                push((int)pop() % (int)op2);
-                break;
-            case '\n':
-                printf("\t%.8g\n", pop());
-                break;
-            default:
-                printf("error: unknown command %s\n", s);
-                break;
+  while((type = getop(s)) != EOF) {
+    switch(type) {
+      case NUMBER:
+        push(atof(s));
+        break;
+      case '+':
+        push(pop() + pop());
+        break;
+      case '*':
+        push(pop() * pop());
+      case '-':
+        op2 = pop();
+        push(pop() - op2);
+        break;
+      case '/':
+        op2 = pop();
+        if (op2 != 0.0) {
+          push(pop() / op2);
+        } else {
+          printf("error: zero divisor\n");
         }
+        break;
+      case '\n':
+        printf("\t%.8g\n", pop());
+        break;
+      default:
+        printf("error: unknown command %s\n", s);
+        break;
     }
-    return 0;
+  }
+  return 0;
 }
 
 int sp = 0;
@@ -62,50 +55,58 @@ double val[MAXVAL];
 
 void push(double f)
 {
-    if (sp < MAXVAL) {
-        val[sp++] = f;
-    } else {
-        printf("error: stack full, can't push %g\n", f);
-    }
+  if (sp < MAXVAL) {
+    val[sp++] = f;
+  } else {
+    printf("error: stack full, can't push %g\n", f);
+  }
 }
 
 double pop(void)
 {
-    if (sp > 0) {
-        return val[--sp];
-    } else {
-        printf("error: stack empty\n");
-        return 0.0;
-    }
+  if (sp > 0) {
+    return val[--sp];
+  } else {
+    printf("error: stack empty\n");
+    return 0.0;
+  }
 }
+
+int getch(void);
+void ungetch(int);
 
 int getop(char s[])
 {
-    int i, c;
+  int i, c;
 
-    while ((s[0] = c = getch()) == ' ' || c == '\t')
-        ;
+  while((s[0] = c = getch()) == ' ' || c == '\t') {
+    ;
+  }
 
-    s[1] = '\0';
-    if (!isdigit(c) && c != '.') {
-        return c;
+  s[1] = '\0';
+  if (!isdigit(c) && c != '.') {
+    return c;
+  }
+
+  i = 0;
+
+  if (isdigit(c)) {
+    while(isdigit(s[i++] = c = getch())) {
+      ;
     }
+  }
 
-    if (isdigit(c)) {
-        while (isdigit(s[++i] = c = getch()))
-            ;
+  if (c == '.') {
+    while (isdigit(s[++i] = c = getch())) {
+      ;
     }
-
-    if (c == '.') {
-        while(isdigit(s[++i] = c = getch()))
-            ;
-    }
-
-    s[i] = '\0';
-    if (c != EOF) {
-        ungetch(c);
-    }
-    return NUMBER;
+  }
+  
+  s[i] = '\0';
+  if (c != EOF) {
+    ungetch(c);
+  }
+  return NUMBER;
 }
 
 char buf[BUFSIZE];
@@ -113,14 +114,16 @@ int bufp = 0;
 
 int getch(void)
 {
-    return (bufp > 0) ? buf[--bufp] : getchar();
+  return (bufp > 0) ? buf[--bufp] : getchar();
 }
 
 void ungetch(int c)
 {
-    if (bufp >= BUFSIZE) {
-        printf("ungetch: too many characters");
-    } else {
-        buf[bufp++] = c;
-    }
+  if (bufp >= BUFSIZE) {
+    printf("ungetch: too many characters\n");
+  } else {
+    buf[bufp++] = c;
+  }
 }
+
+
