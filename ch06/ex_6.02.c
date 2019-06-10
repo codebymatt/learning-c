bb1cfd74a0045972adf292df681f582cbfcda664
codebymatt <mattcraig365@gmail.com>
2019-06-10 20:18:28 +0100

    Finish ex 6.2

---
+++ ch06/ex_6.02.c
@@ -0,0 +1,131 @@
+// Assume variable names start with an alphabet character
+// Counting words in strings, but not in comments
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+#include "getword.h"
+
+#define MAXWORD 100
+
+struct tnode
+{
+  char *word;
+  int count;
+  int new_group;
+  struct tnode *left;
+  struct tnode *right;
+};
+
+char current_group_prefix[MAXWORD];
+int required_len = 6;
+int getword(char *, int);
+struct tnode *addtree(struct tnode *, char *);
+void treeprint(struct tnode *);
+
+int main(int argc, char *argv[])
+{
+  if (argc > 1) {
+    while(--argc > 0) {
+      if (strcmp(*(++argv), "-l")) {
+        required_len = atoi(*argv);
+      }
+    }
+  }
+
+  struct tnode *root;
+  char word[MAXWORD];
+
+  root = NULL;
+  while (getword(word, MAXWORD) != EOF)
+  {
+    if (isalpha(word[0]) && strlen(word) >= required_len)
+    {
+      root = addtree(root, word);
+    }
+  }
+  treeprint(root);
+
+  return 0;
+}
+
+struct tnode *talloc(void);
+char *my_strdup(char *);
+
+struct tnode *addtree(struct tnode *p, char *w)
+{
+  int cond;
+
+  if (p == NULL)
+  {
+    p = talloc();
+    p->word = my_strdup(w);
+    p->count = 1;
+    p->new_group = 0;
+    p->left = p->right = NULL;
+  }
+  else if ((cond = strcmp(w, p->word)) == 0)
+  {
+    p->count++;
+  }
+  else if (cond < 0)
+  {
+    p->left = addtree(p->left, w);
+  }
+  else
+  {
+    p->right = addtree(p->right, w);
+  }
+
+  return p;
+}
+
+char *get_str_prefix(char *);
+
+void treeprint(struct tnode *p)
+{
+  if (p != NULL)
+  {
+    treeprint(p->left);
+    if (strlen(p->word) >= required_len) {
+      if ((strncmp(p->word, current_group_prefix, required_len) != 0)) {
+        strcpy(current_group_prefix, get_str_prefix(my_strdup(p->word)));
+        printf("\nGROUP BEGINNING WITH '%s'\n", current_group_prefix);
+      }
+      printf("%4d %s\n", p->count, p->word);
+    }
+    treeprint(p->right);
+  }
+}
+
+struct tnode *talloc(void)
+{
+  return (struct tnode *)malloc(sizeof(struct tnode));
+}
+
+char *my_strdup(char *s)
+{
+  char *p;
+
+  p = (char *)malloc(strlen(s) + 1);
+  if (p != NULL)
+  {
+    strcpy(p, s);
+  }
+  return p;
+}
+
+char *get_str_prefix(char *original) {
+  char *trimmed;
+  trimmed = (char *)malloc(strlen(original) + 1);
+  int counter = 0;
+  while (counter < required_len) {
+    trimmed[counter++] = *original++;
+  }
+
+  return trimmed;
+}
+
+// int my_strcmp(word) {
+
+// }
---
+++ ch06/getword.h
@@ -0,0 +1,122 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+int getword(char *word, int lim)
+{
+  int c, getch(void);
+  void ungetch(int);
+  char *w = word;
+
+  while (isspace(c = getch()))
+  {
+    ;
+  }
+
+  if (c != EOF)
+  {
+    // SKIP PREPROCESSOR CONTROL LINES
+    // OK to skip lines starting with '#', as only time it's
+    // used outside '#define' is inside string constant.
+    if (c == '#')
+    {
+      while ((c = getch()) != '\n')
+      {
+        ;
+      }
+      *w = '\0';
+      return w[0];
+    }
+    // SKIP COMMENT LINES
+    else if (c == '/')
+    {
+      if ((c = getch()) == '/')
+      {
+        while ((c = getch()) != '\n')
+        {
+          ;
+        }
+        if ((c = getch()) != EOF)
+        {
+          *w++ = c;
+        }
+      }
+      else
+      {
+        ungetch(c);
+        *w++ = '/';
+        *w = '\0';
+        return w[0];
+      }
+    }
+    else
+    {
+      *w++ = c;
+    }
+  }
+
+  // STRING CONSTANTS
+  if (c == '"')
+  {
+    for (; --lim > 0; w++)
+    {
+      if ((*w = getch()) == '"')
+      {
+        w++;
+        break;
+      }
+    }
+  }
+
+  if (c == '\'')
+  {
+    for (; --lim > 0; w++)
+    {
+      if ((*w = getch()) == '\'')
+      {
+        w++;
+        break;
+      }
+    }
+  }
+
+  // NORMAL WORDS
+  if (!isalpha(c))
+  {
+    *w = '\0';
+    return c;
+  }
+
+  for (; --lim > 0; w++)
+  {
+    if (!isalnum(*w = getch()) && (*w != '_'))
+    {
+      ungetch(*w);
+      break;
+    }
+  }
+  *w = '\0';
+  return word[0];
+}
+
+#define BUFSIZE 100
+
+char buf[BUFSIZE];
+int bufp = 0;
+
+int getch(void)
+{
+  return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c)
+{
+  if (bufp >= BUFSIZE)
+  {
+    printf("ungetch: too many characters\n");
+  }
+  else
+  {
+    buf[bufp++] = c;
+  }
+}
