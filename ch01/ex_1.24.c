888bdec9b4c3c76c380887c217a4f72dfa069f80
codebymatt <mattcraig365@gmail.com>
2019-06-13 22:10:15 +0100

    Finish ex 1.24 AND THUS ALL OF CHAPTERS 1 throygh 6

--- ch01/ex_1.24.c
+++ ch01/ex_1.24.c
@@ -1,4 +1,51 @@
-# include <stdio.h>
-# define MAXLENGTH 4096
+// Check for unbalanced parentheses, brackets, and braces only
+#include <stdio.h>
+#define MAXLENGTH 4096
 
 /* WILL RETURN TO THIS EXERCISE */
+
+int main()
+{
+  int c;
+  int brackets, parens, braces;
+  brackets = parens = braces = 0;
+
+  while ((c = getchar()) != EOF)
+  {
+    switch (c)
+    {
+    case '(':
+      parens++;
+      break;
+    case '{':
+      braces++;
+      break;
+    case '[':
+      brackets++;
+      break;
+    case ')':
+      parens--;
+      break;
+    case '}':
+      braces--;
+      break;
+    case ']':
+      brackets--;
+      break;
+    }
+  }
+
+  if (brackets != 0 || braces != 0 || parens != 0)
+  {
+    printf("Unbalanced syntax!\n");
+    printf("\tBrackets: %i\n", brackets);
+    printf("\tBraces: %i\n", braces);
+    printf("\tParentheses: %i\n", parens);
+  }
+  else
+  {
+    printf("Everything looks balanced!\n");
+  }
+
+  return 0;
+}
